11f6276b899a8ab3c518c12e9bc7f8a73bb15e2e
tgwow <thiago.2809@outlook.com.br>
2019-07-12 22:39:01 -0300

    add getUser() /lib/input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 //Here starts the main code
-import MainView from './view/MainView';
+//import MainView from './view/MainView';
+
+const inquirer = require('../src/lib/Input');
 
 const run = async function() {
     //Here comes the execution flow
-    await console.log("yoooo");
+    const user      = await inquirer.getUser();
+    await console.log(user);
 }
 
 run();
--- src/lib/Input.js
+++ src/lib/Input.js
@@ -1,3 +1,23 @@
-export default {
+const inquirer      = require('inquirer');
+
+module.exports = {
     //Input funcions here
+
+    getUser: () => {
+        const questions = [
+            {
+                name: 'username',
+                type: 'input',
+                message: 'Enter your name: ',
+                validate: function (value) {
+                    if (value.lenght) {
+                        return true;
+                    }else {
+                        return true;
+                    }
+                }
+            },
+        ];
+        return inquirer.prompt(questions);
+    },
 }
