a34c103759f9ead7e45d525610e838728d3b4b7a
CarlosSanches07 <carlos-caps@hotmail.com>
2019-07-12 22:21:01 -0300

    user controller getUser

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,5 +1,15 @@
 import Input from '../lib/Input';
 
-export default {
+export default class UserController {
     //User controller functions
+    constructor() {
+        _input = Input;
+    }
+
+    getUser() {
+        const data = _input.getUser();
+        return {
+            name : data[0].name
+        }
+    }
 }
